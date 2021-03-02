f23807840c29c6746917efe59f492d0660845b2e
Jianqi <a1752399@student.adelaide.edu.au>
2021-03-02 14:23:43 +1030

    DFS

---
+++ src/ExampleClass/TreeNode.java
@@ -0,0 +1,28 @@
+package ExampleClass;
+
+public class TreeNode {
+    int val;
+
+    public int getVal() {
+        return val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
